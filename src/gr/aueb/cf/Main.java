d22b8a5c36e81aa8dea4b1fb154ee4d2f5a96284
D. Velaoras <velaorasdimi@gmail.com>
2025-02-16 21:18:59 +0200

    project created

---
+++ src/gr/aueb/cf/Main.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 4;
+        int result;
+
+        result = add(num1, num2);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
